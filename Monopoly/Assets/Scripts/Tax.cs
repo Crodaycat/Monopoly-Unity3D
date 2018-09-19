8d1413d5857df859952191bfc659616784960820
unknown <paulscotch1@gmail.com>
2018-09-18 19:35:12 -0500

    actualizacion 2.0

--- Monopoly/Assets/Scripts/Tax.cs
+++ Monopoly/Assets/Scripts/Tax.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Tax : Square {
     public enum TaxType
     {
-
+        LuxuryTax,
+        IncomeTax
     };
+
+    public TaxType taxType;
+    public int taxValue;
+    
+    public int GetIncomeTax(Player player)
+    {
+        taxValue = TaxType
+        return taxValue;
+    }
 }
